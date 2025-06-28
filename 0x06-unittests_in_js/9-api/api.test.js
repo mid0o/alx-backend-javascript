f225ee2b717123acafccceb059e5592265ff3839
Mohamed Saeed <96616586+mid0o@users.noreply.github.com>
2025-06-28 11:21:46 +0300

    Done

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,29 +1,43 @@
 const request = require('request');
 const { expect } = require('chai');
 
-describe('API Integration Tests', () => {
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
+
   describe('Index page', () => {
-    const url = 'http://localhost:7865';
-    it('should return status code 200', (done) => {
-      request(url, (error, response, body) => {
-        expect(response.statusCode).to.equal(200);
+    it('GET / returns correct status code', (done) => {
+      request.get(`${API_URL}/`, (error, response, body) => {
+        expect(response.statusCode).to.be.equal(200);
+        done();
+      });
+    });
+
+    it('GET / returns correct welcome message', (done) => {
+      request.get(`${API_URL}/`, (error, response, body) => {
+        expect(body).to.be.equal('Welcome to the payment system');
         done();
       });
     });
   });
 
   describe('Cart page', () => {
-    it('should return status 200 when :id is a number', (done) => {
-      request('http://localhost:7865/cart/12', (error, response, body) => {
-        expect(response.statusCode).to.equal(200);
-        expect(body).to.equal('Payment methods for cart 12');
+    it('GET /cart/:id returns correct status code for valid :id', (done) => {
+      request.get(`${API_URL}/cart/12`, (error, response, body) => {
+        expect(response.statusCode).to.be.equal(200);
         done();
       });
     });
 
-    it('should return status 404 when :id is NOT a number', (done) => {
-      request('http://localhost:7865/cart/hello', (error, response, body) => {
-        expect(response.statusCode).to.equal(404);
+    it('GET /cart/:id returns correct content for valid :id', (done) => {
+      request.get(`${API_URL}/cart/12`, (error, response, body) => {
+        expect(body).to.be.equal('Payment methods for cart 12');
+        done();
+      });
+    });
+
+    it('GET /cart/:id returns 404 status code for invalid :id', (done) => {
+      request.get(`${API_URL}/cart/hello`, (error, response, body) => {
+        expect(response.statusCode).to.be.equal(404);
         done();
       });
     });
