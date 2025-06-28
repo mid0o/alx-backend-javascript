85c0e54f4f9227f76facb7ee927c8a0c3431ea14
Mohamed Saeed <mohamedev@outlook.com>
2025-06-27 21:19:29 -0700

    fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,41 @@
-process.stdout.write('Welcome to ALX, what is your name?\n');
+process.stdout.write('Welcome to ALX, what is your name?\n')
 
-process.stdin.on('data', (data) => {
-  const chunk = process.stdin.read(); => {
-    process.stdout.write(`Your name is: ${data}`);
-});
 
-process.stdin.on('end', () => {
+
+.if (process.stdin.isTTY) {
+
+
+
+  process.stdin.on('data', (data) => {
+
+    process.stdout.write(`Your name is: ${data.toString()}`);
+
+    process.exit();
+
+  });
+
+} else {
+
+
+
+  let input = '';
+
+  process.stdin.on('data', (chunk) => {
+
+    input += chunk.toString();
+
+  });
+
+  process.stdin.on('end', () => {
+
+    process.stdout.write(`Your name is: ${input}`);
+
+  });
+
+}
+
+process.on('exit', () => {
+
   process.stdout.write('This important software is now closing\n');
+
 });
